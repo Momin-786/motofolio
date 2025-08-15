0ad35350eb3108abb0b93490c0830b4a8279b279
Momin-786 <momina7863@gmail.com>
2025-08-15 13:19:22 +0500

    add bot

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,16 @@ class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
+                    
+                    {/* Botpress Webchat Integration v3.2 */}
+                    <script 
+                        src="https://cdn.botpress.cloud/webchat/v3.2/inject.js" 
+                        defer 
+                    />
+                    <script 
+                        src="https://files.bpcontent.cloud/2025/08/11/14/20250811140816-HLBM1UDA.js" 
+                        defer 
+                    />
                 </body>
             </Html>
         )
